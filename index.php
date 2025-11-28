aac54415228f837907d329162cd6ff5471482753
alexisdejean <alexis.dejean@campuscarloacutis.fr>
2025-11-28 08:59:25 +0100

    <init>(initialisation du projet et des fichiers sur Github et local)

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+include "includes/header.php";
+include "includes/modele/bdd.php";
+include "includes/modele/fonctions.php";
+include "controleurs/c_principal.php";
+include "includes/footer.php";
+
+?>
